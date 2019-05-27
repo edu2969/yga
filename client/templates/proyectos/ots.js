c3f96a5fcf109ac10eebe8ad28cc41a43d1a0faf
EdU <edtronco@gmail.com>
2019-05-27 00:18:53 -0400

    - Corrección de jornadas - Corrección de cuentas - Demo de OTs

--- client/templates/proyectos/ots.js
+++ client/templates/proyectos/ots.js
@@ -4,3 +4,24 @@ Template.ots.rendered = function() {
     defaultDate: new Date()
   });
 }
+
+Template.ots.helpers({
+    tareas() {
+        return [{}];
+    }
+})
+
+Template.ots.events({
+    "click .boton-simple.label-danger"() {
+        $(".item[id='-1']").toggle();
+    },
+    "click .boton-simple.label-success"() {
+        $(".item[id='-1']").toggle();
+    },
+    "click .boton-simple.glyphicon-edit, click .boton-simple.glyphicon-plus"() {
+        $(".item[id='-1']").toggle();
+    },
+    "click .boton-simple.glyphicon-trash"() {
+        $("#modalconfirmareliminaciontarea").modal("show");
+    }
+})
